f356f4dedb6f66f0d60d598676b629060fa02088
sanjaya <na5703hansitha@gmail.com>
2018-08-27 14:36:13 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+var searchInsta = require('./src/models/searchInsta');
+
+const app = express();
+const port = 9999;
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    console.log('get req came');
+    res.render('index');
+});
+app.post('/search', function(req, res){
+    console.log('search req came');
+    searchInsta.search(req,res);
+});
+
+app.listen(port, function(){
+    console.log('Server is running on port:', port);
+});
